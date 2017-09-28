5db94bbb5c863efdfdc32ba49a8548d0bf6f0f25
Steven <kan02100@naver.com>
2017-09-28 15:31:52 +1300

    Button script added

---
+++ scripts/button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	public Color defaultColour;
+	public Color selectedColour;
+	private Material mat;
+
+	void Start()
+	{
+		mat = GetComponent<Renderer>().material;
+	}
+
+	void OnTouchDown()
+	{
+		mat.color = selectedColour;
+	}
+	void OnTouchUp()
+	{
+		mat.color = defaultColour;
+	}
+	void OnTouchStay()
+	{
+		mat.color = selectedColour;
+	}
+	void OnTouchExit()
+	{
+		mat.color = defaultColour;
+	}
+}
